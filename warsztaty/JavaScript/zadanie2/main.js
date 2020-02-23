a3e1d6b810a3578c317b042f669c9caa08e517c9
pchelka-code <59804185+pchelka-code@users.noreply.github.com>
2020-02-23 16:37:32 +0100

    pobieranie danych z formularza

--- warsztaty/JavaScript/zadanie2/main.js
+++ warsztaty/JavaScript/zadanie2/main.js
@@ -3,7 +3,8 @@ function send (event) {
     // console.log(event)
     let person = {
         imie: document.getElementById('fname').value,
-        nazwisko: document.getElementById('lname').value
+        nazwisko: document.getElementById('lname').value,
+        email: document.getElementById('email').value
     }
     console.log(person)
 
@@ -11,3 +12,13 @@ function send (event) {
 
 let formularz = document.getElementById('formularz')
 formularz.addEventListener('submit', send)
+
+//Pierwsza wersja
+// function changeInput() {
+//     let info = document.getElementById('change').value
+//     console.log ("changeIput -> info", info)
+// }
+
+function changeInput(daneZInputa) {
+    console.log ("changeIput -> info", daneZInputa)
+}
