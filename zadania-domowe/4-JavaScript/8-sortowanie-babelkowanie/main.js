6310be935dd34d4b8717ebe7fe4d524c2fbc8664
pchelka-code <59804185+pchelka-code@users.noreply.github.com>
2020-02-27 22:31:06 +0100

    drobna zminana

--- zadania-domowe/4-JavaScript/8-sortowanie-babelkowanie/main.js
+++ zadania-domowe/4-JavaScript/8-sortowanie-babelkowanie/main.js
@@ -11,7 +11,6 @@ let tablica = [12, 67, 34, 23.01, 24, 2, 56, 8, 0x10, 23.02, 78, 34, 1e2, 45, 67
 
 function babelkowanie (table) {
     let zmiana;
-    let odwrocenie;
     do {
         zmiana = false;
         for (let i=0; i<table.length; i++) {
