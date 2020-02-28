36058ad32979add984c2d42d10bea9b3e8b62d19
pchelka-code <59804185+pchelka-code@users.noreply.github.com>
2020-02-28 13:18:16 +0100

    Dodawanie elementu z kolejnym numerem

---
+++ zadania-domowe/5-Model-DOM/2-dodaj-elementy-listy/main.js
@@ -0,0 +1,19 @@
+let lista = document.getElementById('items')
+
+function dodanieItemu () {
+    let nowyItem = document.createElement('li');
+    let liczbaItemow = document.getElementsByClassName('item').length;
+    nowyItem.className = 'item';
+    nowyItem.innerText = 'Item ' + (liczbaItemow+1);
+    lista.appendChild(nowyItem);
+}
+
+let btnMain = document.getElementById('btn')
+btnMain.addEventListener('click', dodanieItemu)
+
+// Napisz skrypt, który po kliknięciu w przycisk doda kolejny element do listy z nazwą zakończoną liczbą kolejnego elementu - np. Item 4, Item 5, Item 6 itd.
+// Każdemu z elementów listy ustaw klasę "item"
+// Podpowiedź przed wysłaniem zadania do sprawdzenia:
+// - napisz funkcję obsługującą klik przycisku button
+// - musisz pobrać obecną ilość item'ów w liście, aby dodać numer kolejnego
+// - musisz stworzyć w JavaScript nowy element w HTML i dodać go do listy używając odpowiedniej do tego metody w JS :)
