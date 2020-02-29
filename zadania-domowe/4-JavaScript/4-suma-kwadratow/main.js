7dd1187d52d5ab0d3d326207490305a33165ae22
pchelka-code <59804185+pchelka-code@users.noreply.github.com>
2020-02-29 09:40:05 +0100

    Inna wersja suma kwadratów

--- zadania-domowe/4-JavaScript/4-suma-kwadratow/main.js
+++ zadania-domowe/4-JavaScript/4-suma-kwadratow/main.js
@@ -10,3 +10,32 @@ function sumaKwadrat(zmienna) {
 }
 
 sumaKwadrat(tablica);
+
+
+// Inna wersja
+let num = [0,1,2,3,4,5];
+
+function sumaKwadratow () {
+    let kwadrat = 0;
+    let suma = 0;
+    for(let i=0; i< num.length; i++) {
+        kwadrat += Math.pow(num[i],2);
+    }
+    return kwadrat
+}
+
+let wynik = sumaKwadratow()
+console.log(wynik);
+
+// Wersja z forEach JS6 (nowsza wersja)
+
+function sumaKwadratow2 () {
+    let kwadrat = 0
+    num.forEach(item => {
+        kwadrat += Math.pow(item,2)
+    })
+    return kwadrat
+}
+
+let wynik2 = sumaKwadratow2()
+console.log(wynik2)
