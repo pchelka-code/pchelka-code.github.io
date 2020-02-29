a8a582605a9c500f44cebaadbaf25183dc6330aa
pchelka-code <59804185+pchelka-code@users.noreply.github.com>
2020-02-29 10:05:06 +0100

    Inna wersja ciągu fibo

--- zadania-domowe/4-JavaScript/5-element-fibonacci/main.js
+++ zadania-domowe/4-JavaScript/5-element-fibonacci/main.js
@@ -23,3 +23,16 @@ function fibo(n) {
 }
 
 fibo(11);
+
+
+// Inna wersja
+let num = [0,1]
+let wynik = [0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987]
+
+function fibo2 (n) {
+    let nowy = num[num.length-1] + num[num.length -2]
+    num.push(nowy)
+}
+console.log(num[num.length-1])
+
+fibo2(12)
