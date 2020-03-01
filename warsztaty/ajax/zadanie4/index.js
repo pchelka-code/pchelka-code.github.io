e4d665af07b1ba4025ba1bbdcdff764a4d6a2f31
pchelka-code <59804185+pchelka-code@users.noreply.github.com>
2020-03-01 16:35:34 +0100

    krótsza wersja

--- warsztaty/ajax/zadanie4/index.js
+++ warsztaty/ajax/zadanie4/index.js
@@ -3,24 +3,44 @@
 // pobiera 
 
 
+// function polski () {
+//     let elem1 = document.getElementById('text1')
+//     elem1.innerText = 'Strona Główna'
+//     let elem2 = document.getElementById('text2')
+//     elem2.innerText = 'O mnie'
+//     let elem3 = document.getElementById('text3')
+//     elem3.innerText = 'Galeria'
+//     let elem4 = document.getElementById('text4')
+//     elem4.innerText = 'Kontak'
+// }
+
+// function angielski () {
+//     let elem1 = document.getElementById('text1')
+//     elem1.innerText = 'Main Page'
+//     let elem2 = document.getElementById('text2')
+//     elem2.innerText = 'About'
+//     let elem3 = document.getElementById('text3')
+//     elem3.innerText = 'Gortfolio'
+//     let elem4 = document.getElementById('text4')
+//     elem4.innerText = 'Contact'
+// }
+
+// spróbujemy krótszą wersję:
+let elem1 = document.getElementById('text1')
+let elem2 = document.getElementById('text2')
+let elem3 = document.getElementById('text3')
+let elem4 = document.getElementById('text4')
+
 function polski () {
-    let elem1 = document.getElementById('text1')
     elem1.innerText = 'Strona Główna'
-    let elem2 = document.getElementById('text2')
     elem2.innerText = 'O mnie'
-    let elem3 = document.getElementById('text3')
     elem3.innerText = 'Galeria'
-    let elem4 = document.getElementById('text4')
     elem4.innerText = 'Kontak'
 }
 
 function angielski () {
-    let elem1 = document.getElementById('text1')
     elem1.innerText = 'Main Page'
-    let elem2 = document.getElementById('text2')
     elem2.innerText = 'About'
-    let elem3 = document.getElementById('text3')
     elem3.innerText = 'Gortfolio'
-    let elem4 = document.getElementById('text4')
     elem4.innerText = 'Contact'
 }
