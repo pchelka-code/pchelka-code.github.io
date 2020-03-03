b30010a42b3554e5281a5f6a99f43b3ae333cc0e
pchelka-code <59804185+pchelka-code@users.noreply.github.com>
2020-03-03 12:23:17 +0100

    Zadania domowe jQuery

---
+++ zadania-domowe/6-jQuery/1-animacja-jquery/main.js
@@ -0,0 +1,18 @@
+$(function() {
+
+$('#btn').click(function(){
+    $('#kwadrat').animate({
+        left: '100px',
+        height: '100px',
+        width: '100px',
+    }, 3000, function() {
+        $('#kwadrat').animate({
+            backgroundColor: 'blue'
+        }, 5000, function() {
+            $('#kwadrat').html('<h2>Animacja zakończona</h2>');
+        });
+    });
+})
+
+
+});
